1a931be80466d144aee41d8f625fa92c536bd8e3
tanaikech <tanaike@hotmail.com>
2018-06-15 10:31:42 +0900

    1st commit

---
+++ FilesApp.js
@@ -0,0 +1,432 @@
+/**
+ * GitHub  https://github.com/tanaikech/FilesApp<br>
+ * getFilesAndFoldersInFolder method for FilesApp.<br>
+ * - Retrieve files and folders just under a folder with folderId.
+ * @param {String} folderId folderId.
+ * @param {Object} mimeType One dimensional Array including mimeType you want to retrieve.
+ * @param {String} fields fields which can be used at drive.files.list.
+ * @return {Object} Return Object
+ */
+function getFilesAndFoldersInFolder(folderId, mimeType, fields) {
+  var fa = new FilesApp();
+  return fa.getFilesAndFoldersInFolder(folderId, mimeType, fields);
+}
+
+/**
+ * getAllFoldersInFolder method for FilesApp.<br>
+ * - Retrieve all folders of all level under folderId.
+ * @param {string} folderId folderId
+ * @return {Object} Return Object
+ */
+function getAllFoldersInFolder(folderId) {
+  var fa = new FilesApp();
+  return fa.getAllFoldersInFolder(folderId);
+}
+
+/**
+ * getAllInFolder method for FilesApp.<br>
+ * - Retrieve all files and folders of all level under folderId. All files and folders are included in an array without the level.
+ * @param {string} folderId folderId
+ * @param {Object} mimeType One dimensional Array including mimeType you want to retrieve.
+ * @param {String} fields fields which can be used at drive.files.list.
+ * @return {Object} Return Object
+ */
+function getAllInFolder(folderId, mimeType, fields) {
+  var fa = new FilesApp();
+  return fa.getAllInFolder(folderId, mimeType, fields);
+}
+
+/**
+ * createTree method for FilesApp.<br>
+ * - Create a file and folder tree. Retrieve all folders of all level under folderId. All files and folders are included in an array with the level.
+ * @param {string} folderId Retrieve all folders of all level under folderId.
+ * @param {Object} mimeType One dimensional Array including mimeType you want to retrieve.
+ * @param {String} fields fields which can be used at drive.files.list.
+ * @return {Object} Return Object
+ */
+function createTree(folderId, mimeType, fields) {
+  var fa = new FilesApp();
+  return fa.createTree(folderId, mimeType, fields);
+}
+;
+(function(r) {
+  var FilesApp;
+  FilesApp = (function() {
+    var checkFields, createBatchRequests, createQ, createRequests, getAllFoldersInFolderMain, getFilesByAPIByFetchAll, getFilesByAPIByFetchAllforNoBatch, getFilesByAPIInit, getFilesInFolder, getQueryFromMimeTypes, getRoot, objToQueryParams, parseFilesByFolderList, parseResFromBatchRequests, singleReq;
+
+    FilesApp.name = "FilesApp";
+
+    function FilesApp() {
+      this.url = "https://www.googleapis.com/drive/v3/files";
+      this.fields = "";
+      this.pageSize = 1000;
+      this.headers = {
+        Authorization: 'Bearer ' + ScriptApp.getOAuthToken()
+      };
+      this.maxSearchFolders = 20;
+      this.maxBatchRequests = 100;
+    }
+
+    FilesApp.prototype.getFilesAndFoldersInFolder = function(parent, mimeTypeList, fields) {
+      var mtlq, query;
+      if (!parent || parent === "") {
+        throw new Error("Folder ID was not found.");
+      }
+      mtlq = getQueryFromMimeTypes.call(this, mimeTypeList);
+      parent = parent === "root" ? getRoot.call(this) : parent;
+      query = {
+        maxResults: this.pageSize,
+        q: mtlq !== "" ? "'" + parent + "' in parents" + " and " + mtlq + " and trashed=false" : "'" + parent + "' in parents and trashed=false",
+        fields: fields
+      };
+      return singleReq.call(this, query);
+    };
+
+    FilesApp.prototype.getAllFoldersInFolder = function(parent) {
+      var allFolders, query;
+      if (!parent || parent === "") {
+        throw new Error("Folder ID was not found.");
+      }
+      parent = parent === "root" ? getRoot.call(this) : parent;
+      query = {
+        pageSize: this.pageSize,
+        q: "mimeType='application/vnd.google-apps.folder'",
+        fields: "files(id,name,parents),nextPageToken"
+      };
+      allFolders = singleReq.call(this, query);
+      return getAllFoldersInFolderMain.call(this, parent, DriveApp.getFileById(parent).getName(), allFolders);
+    };
+
+    FilesApp.prototype.getAllInFolder = function(parent, mimeTypes, fields) {
+      var folderList;
+      this.fields = fields;
+      folderList = this.getAllFoldersInFolder(parent);
+      return getFilesInFolder.call(this, folderList.id, mimeTypes);
+    };
+
+    FilesApp.prototype.createTree = function(parent, mimeTypes, fields) {
+      var allFiles, folderList;
+      folderList = this.getAllFoldersInFolder(parent);
+      this.fields = checkFields.call(this, fields);
+      allFiles = getFilesInFolder.call(this, folderList.id, mimeTypes);
+      return parseFilesByFolderList.call(this, allFiles, folderList);
+    };
+
+    getAllFoldersInFolderMain = function(folderId, folderName, folderList) {
+      return (function() {
+        var c;
+        return (c = function(folder, folderName, folderSt, res) {
+          var ar, arrayFolderSt;
+          ar = folderList.filter(function(e) {
+            return e.parents[0] === folder;
+          });
+          folderSt += folderName + "#_foohoge_#" + folder + "#_aabbccddee_#";
+          arrayFolderSt = folderSt.split("#_aabbccddee_#");
+          arrayFolderSt.pop();
+          res.name.push(arrayFolderSt.map(function(e) {
+            return e.split("#_foohoge_#")[0];
+          }));
+          res.id.push(arrayFolderSt.map(function(e) {
+            return e.split("#_foohoge_#")[1];
+          }));
+          ar.length === 0 && (folderSt = "");
+          ar.forEach(function(e) {
+            c(e.id, e.name, folderSt, res);
+          });
+          return res;
+        })(folderId, folderName, "", {
+          id: [],
+          name: []
+        });
+      })();
+    };
+
+    getFilesInFolder = function(folderList, mimeTypeList) {
+      var fl, folLen, i, k, mtlq, offset, qs, ref, rq, sep, urls;
+      if (folderList.length === 0) {
+        throw new Error("No folderList.");
+      }
+      fl = folderList.map(function(e) {
+        return e[e.length - 1];
+      });
+      mtlq = getQueryFromMimeTypes.call(this, mimeTypeList);
+      qs = [];
+      folLen = fl.length;
+      if (folLen > this.maxSearchFolders) {
+        sep = Math.floor(folLen / this.maxSearchFolders);
+        sep = folLen % this.maxSearchFolders > 0 ? sep + 1 : sep;
+        for (i = k = 0, ref = sep; 0 <= ref ? k < ref : k > ref; i = 0 <= ref ? ++k : --k) {
+          offset = i * this.maxSearchFolders;
+          qs.push(createQ(fl.slice(offset, offset + this.maxSearchFolders), mtlq));
+        }
+      } else {
+        qs.push(createQ(fl, mtlq));
+      }
+      urls = getFilesByAPIInit.call(this, qs);
+      rq = createRequests.call(this, urls);
+      return getFilesByAPIByFetchAll.call(this, rq[0], [], rq[1]);
+    };
+
+    createRequests = function(urls) {
+      var i, k, offset, ref, reqForFetchApp, reqs, sep, urlBk, urlsLen, urlss;
+      reqForFetchApp = [];
+      urlBk = [];
+      urlsLen = urls.length;
+      if (urlsLen < this.maxBatchRequests) {
+        urlBk.push(urls);
+        reqs = createBatchRequests.call(this, urls);
+        reqForFetchApp.push(reqs);
+      } else {
+        sep = Math.floor(urlsLen / this.maxBatchRequests);
+        sep = urlsLen % this.maxBatchRequests > 0 ? sep + 1 : sep;
+        for (i = k = 0, ref = sep; 0 <= ref ? k < ref : k > ref; i = 0 <= ref ? ++k : --k) {
+          offset = i * this.maxBatchRequests;
+          urlss = urls.slice(offset, offset + this.maxBatchRequests);
+          urlBk.push(urlss);
+          reqs = createBatchRequests.call(this, urlss);
+          reqForFetchApp.push(reqs);
+        }
+      }
+      return [reqForFetchApp, urlBk];
+    };
+
+    getFilesByAPIInit = function(qs) {
+      return qs.map((function(_this) {
+        return function(q) {
+          var query;
+          query = {
+            pageSize: _this.pageSize,
+            q: q,
+            fields: _this.fields
+          };
+          return _this.url + "?" + (objToQueryParams.call(_this, query));
+        };
+      })(this));
+    };
+
+    singleReq = function(query) {
+      var params, req;
+      params = objToQueryParams.call(this, query);
+      req = {
+        method: "get",
+        url: this.url + "?" + params,
+        headers: this.headers,
+        muteHttpExceptions: true
+      };
+      return getFilesByAPIByFetchAllforNoBatch.call(this, [req], []);
+    };
+
+    getFilesByAPIByFetchAll = function(reqs, fileList, urlBk) {
+      var err, ntoken, response, rq, urls;
+      response = UrlFetchApp.fetchAll(reqs);
+      err = response.filter(function(e) {
+        return e.getResponseCode() !== 200;
+      });
+      if (err.length > 0) {
+        throw new Error(err.length + " errors occurred. ErrorMessage: " + err.toString());
+        return;
+      }
+      ntoken = [];
+      response.forEach(function(e, i) {
+        var res;
+        res = parseResFromBatchRequests.call(this, e.getContentText());
+        res.forEach(function(f, j) {
+          var token;
+          if (f.status !== 200) {
+            throw new Error("It's incomplete data. Status is " + f.status);
+            return;
+          }
+          Array.prototype.push.apply(fileList, f.object.files);
+          token = f.object.nextPageToken;
+          if (token) {
+            ntoken.push([i, j, encodeURIComponent(token)]);
+          }
+        });
+      });
+      if (ntoken.length > 0) {
+        urls = ntoken.map(function(e) {
+          var url;
+          url = urlBk[e[0]][e[1]];
+          if (~url.indexOf("pageToken=")) {
+            url = url.replace(/pageToken=[\\W]+?(?=[&|\n])/, "pageToken=" + e[2]);
+          } else {
+            url += "&pageToken=" + e[2];
+          }
+          return url;
+        });
+        rq = createRequests.call(this, urls);
+        getFilesByAPIByFetchAll.call(this, rq[0], fileList, rq[1]);
+      }
+      return fileList;
+    };
+
+    getFilesByAPIByFetchAllforNoBatch = function(reqs, fileList) {
+      var err, ntoken, reqss, res;
+      res = UrlFetchApp.fetchAll(reqs);
+      err = res.filter(function(e) {
+        return e.getResponseCode() !== 200;
+      });
+      if (err.length > 0) {
+        throw new Error(err.length + " errors occurred. ErrorMessage: " + err.toString());
+        return;
+      }
+      ntoken = [];
+      res.forEach(function(e, i) {
+        var token;
+        r = JSON.parse(e.getContentText());
+        Array.prototype.push.apply(fileList, r.files);
+        token = r.nextPageToken;
+        if (token) {
+          ntoken.push([i, encodeURIComponent(token)]);
+        }
+      });
+      if (ntoken.length > 0) {
+        reqss = ntoken.map(function(e) {
+          var req;
+          req = reqs.filter(function(f, j) {
+            return j === e[0];
+          });
+          if (~req[0].url.indexOf("pageToken=")) {
+            req[0].url = req[0].url.replace(/pageToken=[\\W]+?(?=[&|\n])/, "pageToken=" + e[1]);
+          } else {
+            req[0].url += "&pageToken=" + e[1];
+          }
+          return req[0];
+        });
+        getFilesByAPIByFetchAll.call(this, reqss, fileList);
+      }
+      return fileList;
+    };
+
+    getRoot = function() {
+      return DriveApp.getRootFolder().getId();
+    };
+
+    createQ = function(fl, mtlq) {
+      var ffs, ffsq;
+      ffs = fl.map(function(e) {
+        return "'" + e + "' in parents";
+      });
+      ffsq = "(" + ffs.join(" or ") + ")";
+      if (mtlq !== "") {
+        return ffsq + " and " + mtlq + " and trashed=false";
+      } else {
+        return ffsq + " and trashed=false";
+      }
+    };
+
+    objToQueryParams = function(query) {
+      return Object.keys(query).filter(function(e) {
+        return query[e];
+      }).map(function(e) {
+        return e + "=" + encodeURIComponent(query[e]);
+      }).join("&");
+    };
+
+    getQueryFromMimeTypes = function(mimeTypeList) {
+      var mimetypes;
+      if (!mimeTypeList || mimeTypeList.length === 0) {
+        mimeTypeList = ["\*"];
+      }
+      if (mimeTypeList.indexOf("\*") > -1) {
+        mimetypes = "";
+      } else {
+        mimetypes = mimeTypeList.map(function(e) {
+          return "mimeType='" + e + "'";
+        });
+      }
+      if (mimetypes !== "") {
+        return "(" + mimetypes.join(" or ") + ")";
+      } else {
+        return "";
+      }
+    };
+
+    checkFields = function(fields) {
+      var pos;
+      if (fields === "" || fields === "undefined" || (fields == null)) {
+        fields = "files(id,name,parents),nextPageToken,kind";
+      } else {
+        if (!~fields.indexOf("nextPageToken")) {
+          fields = fields + ",nextPageToken";
+        }
+        if ((~fields.indexOf("(") && ~fields.indexOf(")")) && !~fields.indexOf("parents")) {
+          pos = fields.indexOf(")");
+          fields = fields.slice(0, pos) + ",parents" + fields.slice(pos, fields.length);
+        }
+      }
+      return fields;
+    };
+
+    parseFilesByFolderList = function(allFiles, folderList) {
+      var allValues, filesLen, foldersLen;
+      filesLen = allFiles.length;
+      foldersLen = folderList.id.length;
+      if (filesLen === 0) {
+        return {
+          folderTree: folderList,
+          filesInFolder: []
+        };
+      }
+      allValues = folderList.id.map(function(e, i) {
+        return {
+          folderTreeById: e,
+          folderTreeByName: folderList.name[i],
+          filesInFolder: allFiles.filter(function(f) {
+            var ok;
+            ok = f.parents.filter(function(g) {
+              return g === e[e.length - 1];
+            });
+            return ok.length > 0;
+          })
+        };
+      });
+      return {
+        topFolderId: folderList.id[0],
+        topFolderName: folderList.name[0],
+        totalFilesAndFolders: filesLen,
+        totalFiles: filesLen - foldersLen,
+        totalFolders: foldersLen,
+        files: allValues
+      };
+    };
+
+    createBatchRequests = function(endpoints) {
+      var boundary, contentId, data, lb;
+      boundary = "xxxxxFilesAppxxxxx";
+      lb = "\r\n";
+      contentId = 0;
+      data = "--" + boundary + lb;
+      endpoints.forEach(function(endpoint) {
+        data += "Content-Type: application/http" + lb;
+        data += "Content-ID: " + ++contentId + lb + lb;
+        data += "GET " + endpoint + lb + lb;
+        data += "--" + boundary + lb;
+      });
+      return {
+        method: "post",
+        url: "https://www.googleapis.com/batch/drive/v3",
+        headers: this.headers,
+        contentType: "multipart/mixed; boundary=" + boundary,
+        payload: data,
+        muteHttpExceptions: true
+      };
+    };
+
+    parseResFromBatchRequests = function(res) {
+      var splittedRes;
+      splittedRes = res.split("--batch");
+      return splittedRes.slice(1, splittedRes.length - 1).map(function(e) {
+        return {
+          contentId: Number(e.match(/Content-ID: response-(\d+)/)[1]),
+          status: Number(e.match(/HTTP\/\d+.\d+ (\d+)/)[1]),
+          object: JSON.parse(e.match(/{[\S\s]+}/)[0])
+        };
+      });
+    };
+
+    return FilesApp;
+
+  })();
+  return r.FilesApp = FilesApp;
+})(this);
---
+++ README.md
@@ -0,0 +1,270 @@
+FilesApp
+=====
+
+<a name="TOP"></a>
+[![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENCE)
+
+<a name="Overview"></a>
+# Overview
+**FilesApp is a GAS library for retrieving file and folder list in Google Drive using Google Apps Script (GAS). Also this can create a tree from all files and folders in Google Drive.**
+
+
+<a name="Description"></a>
+# Description
+When I create some applications using Google Drive, there are often the case which is required to retrieve the file list and folder list. I had prepared the script each time for each case so far. But recently, I thought that if there is a library for retrieving the file and folder list (as a tree), it will be useful for me and other developers. So I created this. If this was useful for your situation, I'm glad.
+
+
+# Library's project key
+~~~
+1dBxqh6gpWqFv4h0M25ALp4bhcs6CoqhiIJ0fUeUjR57bAxIN2PqfkQns
+~~~
+
+
+<a name="Howtoinstall"></a>
+# How to install
+In order to use this library, please install this as a library.
+
+1. [Install FilesApp library](https://developers.google.com/apps-script/guides/libraries).
+    - Library's project key is **``1dBxqh6gpWqFv4h0M25ALp4bhcs6CoqhiIJ0fUeUjR57bAxIN2PqfkQns``**.
+1. FilesApp uses Drive API. Please enable Drive API at API console.
+    - On the Script Editor
+        - Resources -> Cloud Platform Project
+        - Click the lower part of "This script is currently associated with project:"
+    - On "API APIs&services"
+        - In "Getting Started", Click "Enable APIs and get credentials like keys".
+        - Click Library at left side.
+        - At "Search APIs and services", Input "drive api", Click it.
+        - Enable "Google Drive API"
+
+> Recently, when it enabled APIs, I had an experience that I was made to wait for several minutes for enabling APIs. So when you enabled APIs at API console, if the error related to APIs occurs, please run again after several minutes.
+
+
+<a name="Usage"></a>
+# Usage
+There are 4 methods in this library.
+
+| Method | Description |
+|:---|:---|
+| createTree(folderId, mimeType, fields) | Create a file and folder tree. Retrieve all folders of all level under folderId. All files and folders are included in an array with the level. |
+| getAllInFolder(folderId, mimeType, fields) | Retrieve all files and folders of all level under folderId. All files and folders are included in an array without the level. |
+| getAllFoldersInFolder(folderId) | Retrieve all folders of all level under folderId. |
+| getFilesAndFoldersInFolder(folderId, mimeType, fields) | Retrieve files and folders just under a folder with folderId. |
+
+#### Arguments
+- **folderId** : Folder ID of top folder you want to retrieve. Please use as a string.
+    - If you want to retrieve the values under "root", please use "root" for this.
+- **mimeType** : mimeType of files you want to retrieve. Please use as an array like ``["application/vnd.google-apps.script", "image/png"]``
+    - If you use ``null`` for this, all kinds of files are retrieved.
+- **fields** : fields of files you want to retrieve. Please use as a string. This value is the same to ``fields`` which is used at [files.list of Drive API](https://developers.google.com/drive/api/v3/reference/files/list).
+    - If you want to retrieve the filename and ID of files, you can use ``"files(id,name)"``
+
+
+# Samples
+In order to explain the following sample script, it supposes that there is this structure in Google Drive. In this case, it doesn't need that "sampleFolder1" is root.
+
+![](images/fig1.png)
+
+## 1. createTree(folderId, mimeType, fields)
+``folderId`` is the ID of "sampleFolder1". In this sample, the filenames of files with all mimeTypes are retrieved.
+
+**When "root" is used for "folderId", all files of Google Drive can be retrieved.**
+
+~~~javascript
+var res = FilesApp.createTree(folderId, null, "files(name)");
+~~~
+
+##### Result
+The index of the array of ``folderTreeById`` and ``folderTreeByName`` is corresponding to the level of folders.
+
+~~~json
+{
+  "topFolderId": ["### FolderId of sampleFolder1 ###"],
+  "topFolderName": ["sampleFolder1"],
+  "totalFilesAndFolders": 11,
+  "totalFiles": 4,
+  "totalFolders": 7,
+  "files": [
+    {
+      "folderTreeById": ["### FolderId of sampleFolder1 ###"],
+      "folderTreeByName": ["sampleFolder1"],
+      "filesInFolder": [
+        {
+          "name": "Spreadsheet1",
+          "parents": ["### FolderId of sampleFolder1 ###"]
+        },
+        {
+          "name": "sampleFolder_2b",
+          "parents": ["### FolderId of sampleFolder1 ###"]
+        },
+        {
+          "name": "sampleFolder_2a",
+          "parents": ["### FolderId of sampleFolder1 ###"]
+        }
+      ]
+    },
+    {
+      "folderTreeById": ["### FolderId of sampleFolder1 ###", "### FolderId of sampleFolder_2b ###"],
+      "folderTreeByName": ["sampleFolder1", "sampleFolder_2b"],
+      "filesInFolder": [
+        {
+          "name": "Spreadsheet4",
+          "parents": ["### FolderId of sampleFolder_2b ###"]
+        },
+        {
+          "name": "sampleFolder_2b_3b",
+          "parents": ["### FolderId of sampleFolder_2b ###"]
+        },
+        {
+          "name": "sampleFolder_2b_3a",
+          "parents": ["### FolderId of sampleFolder_2b ###"]
+        }
+      ]
+    },
+    {
+      "folderTreeById": ["### FolderId of sampleFolder1 ###", "### FolderId of sampleFolder_2b ###", "### FolderId of sampleFolder_2b_3b ###"],
+      "folderTreeByName": ["sampleFolder1", "sampleFolder_2b", "sampleFolder_2b_3b"],
+      "filesInFolder": [
+        {
+          "name": "sampleFolder_2b_3b_4a",
+          "parents": ["### FolderId of sampleFolder_2b_3b ###"]
+        }
+      ]
+    },
+    {
+      "folderTreeById": ["### FolderId of sampleFolder1 ###", "### FolderId of sampleFolder_2b ###", "### FolderId of sampleFolder_2b_3b ###", "### FolderId of sampleFolder_2b_3a ###"],
+      "folderTreeByName": ["sampleFolder1", "sampleFolder_2b", "sampleFolder_2b_3b", "sampleFolder_2b_3b_4a"],
+      "filesInFolder": [
+        {
+          "name": "Spreadsheet5",
+          "parents": ["### FolderId of sampleFolder_2b_3a ###"]
+        }
+      ]
+    },
+    {
+      "folderTreeById": [ "### FolderId of sampleFolder1 ###", "### FolderId of sampleFolder_2b ###", "### FolderId of sampleFolder_2b_3a ###"],
+      "folderTreeByName": ["sampleFolder1", "sampleFolder_2b", "sampleFolder_2b_3a"],
+      "filesInFolder": [
+        {
+          "name": "Spreadsheet3",
+          "parents": ["### FolderId of sampleFolder_2b_3a ###"]
+        }
+      ]
+    },
+    {
+      "folderTreeById": ["### FolderId of sampleFolder1 ###", "### FolderId of sampleFolder_2a ###"],
+      "folderTreeByName": ["sampleFolder1", "sampleFolder_2a"],
+      "filesInFolder": [
+        {
+          "name": "Spreadsheet2",
+          "parents": ["### FolderId of sampleFolder_2a ###"]
+        },
+        {
+          "name": "sampleFolder_2a_3a",
+          "parents": ["### FolderId of sampleFolder_2a ###"]
+        }
+      ]
+    },
+    {
+      "folderTreeById": ["### FolderId of sampleFolder1 ###", "### FolderId of sampleFolder_2a ###", "### FolderId of sampleFolder_2a_3a ###"],
+      "folderTreeByName": ["sampleFolder1", "sampleFolder_2a", "sampleFolder_2a_3a"],
+      "filesInFolder": []
+    }
+  ]
+}
+~~~
+
+## 2. getAllInFolder(folderId, mimeType, fields)
+
+~~~javascript
+var res = FilesApp.getAllInFolder(folderId, null, "files(name)");
+~~~
+
+##### Result
+All files and folders in a folder you set are retrieved. But it is not as a tree. The speed is faster than ``createTree()``.
+
+~~~json
+[
+    {"name":"Spreadsheet5"},
+    {"name":"Spreadsheet4"},
+    {"name":"Spreadsheet3"},
+    {"name":"Spreadsheet2"},
+    {"name":"Spreadsheet1"},
+    {"name":"sampleFolder_2b_3b_4a"},
+    {"name":"sampleFolder_2a_3a"},
+    {"name":"sampleFolder_2b"},
+    {"name":"sampleFolder_2a"},
+    {"name":"sampleFolder_2b_3b"},
+    {"name":"sampleFolder_2b_3a"}
+]
+~~~
+
+## 3. getAllFoldersInFolder(folderId)
+
+~~~javascript
+var res = FilesApp.getAllFoldersInFolder(folderId);
+~~~
+
+##### Result
+All folders in a folder you set are retrieved as a tree.
+
+~~~json
+{
+    "id":[
+        ["### ID of sampleFolder1 ###"],
+        ["### ID of sampleFolder1 ###","### ID of sampleFolder_2b ###"],
+        ["### ID of sampleFolder1 ###","### ID of sampleFolder_2b ###","### ID of sampleFolder_2b_3b ###"],
+        ["### ID of sampleFolder1 ###","### ID of sampleFolder_2b ###","### ID of sampleFolder_2b_3b ###","### ID of sampleFolder_2b_3b_4a ###"],
+        ["### ID of sampleFolder1 ###","### ID of sampleFolder_2b ###","### ID of sampleFolder_2b_3a ###"],
+        ["### ID of sampleFolder1 ###","### ID of sampleFolder_2a ###"],
+        ["### ID of sampleFolder1 ###","### ID of sampleFolder_2a ###","### ID of sampleFolder_2a_3a ###"]
+    ],
+    "name":[
+        ["sampleFolder1"],
+        ["sampleFolder1","sampleFolder_2b"],
+        ["sampleFolder1","sampleFolder_2b","sampleFolder_2b_3b"],
+        ["sampleFolder1","sampleFolder_2b","sampleFolder_2b_3b","sampleFolder_2b_3b_4a"],
+        ["sampleFolder1","sampleFolder_2b","sampleFolder_2b_3a"],
+        ["sampleFolder1","sampleFolder_2a"],
+        ["sampleFolder1","sampleFolder_2a","sampleFolder_2a_3a"]
+    ]
+}
+~~~
+
+
+## 4. getFilesAndFoldersInFolder(folderId, mimeType, fields)
+
+~~~javascript
+var res = FilesApp.getFilesAndFoldersInFolder(folderId, null, "files(name)");
+~~~
+
+##### Result
+All files and folders just under a folder you set are retrieved.
+
+~~~json
+[
+    {"name":"Spreadsheet1"},
+    {"name":"sampleFolder_2b"},
+    {"name":"sampleFolder_2a"}
+]
+~~~
+
+-----
+
+<a name="Licence"></a>
+# Licence
+[MIT](LICENCE)
+
+<a name="Author"></a>
+# Author
+[Tanaike](https://tanaikech.github.io/about/)
+
+If you have any questions and commissions for me, feel free to tell me.
+
+<a name="Update_History"></a>
+# Update History
+* v1.0.0 (June 15, 2018)
+
+    1. Initial release.
+
+
+[TOP](#TOP)
